6aec1fe711209a0854a0355c14543420077eb165
lidiagaldino <lidiagabrielly14@gmail.com>
2024-07-21 19:04:13 -0300

    refactor: rename username to name in User entity
    - Renamed the username field to name in the User entity.
    - Updated annotations to reflect the new field name.

--- src/main/java/com/lidiagaldino/finances/domain/entities/User.java
+++ src/main/java/com/lidiagaldino/finances/domain/entities/User.java
@@ -23,8 +23,8 @@ public class User {
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   
-  @Column(unique = true, nullable = false)
-  private String username;
+  @Column(nullable = false)
+  private String name;
   @Column(nullable = false)
   private String password;
   @Column(unique = true, nullable = false)
