2356ad1afe07161f3b873d76c6ed8467486cbb63
lidiagaldino <lidiagabrielly14@gmail.com>
2024-07-25 13:50:18 -0300

    feat: implement FindExpenseCategoryById use case
    - Added FindExpenseCategoryById service to handle retrieval of ExpenseCategory by ID
    - Includes error handling for not found categories

---
+++ src/main/java/com/lidiagaldino/finances/application/usecases/expenseCategory/FindExpenseCategoryById.java
@@ -0,0 +1,22 @@
+package com.lidiagaldino.finances.application.usecases.expenseCategory;
+
+import org.springframework.stereotype.Service;
+
+import com.lidiagaldino.finances.domain.entities.ExpenseCategory;
+import com.lidiagaldino.finances.domain.repositories.ExpenseCategoryRepository;
+import com.lidiagaldino.finances.domain.usecases.expenseCategory.FindExpenseCategoryByIdUsecase;
+
+@Service
+public class FindExpenseCategoryById implements FindExpenseCategoryByIdUsecase{
+  private final ExpenseCategoryRepository expenseCategoryRepository;
+
+  public FindExpenseCategoryById(ExpenseCategoryRepository expenseCategoryRepository) {
+    this.expenseCategoryRepository = expenseCategoryRepository;
+  }
+  @Override
+  public ExpenseCategory showExpenseCategory(Integer id) {
+    var expenseCategory = expenseCategoryRepository.showExpenseCategory(id);
+    if(expenseCategory.isEmpty()) throw new RuntimeException("expense category not found");
+    return expenseCategory.get();
+  }
+}
