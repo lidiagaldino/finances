6fb28d33f729ebdc8d9b185a2e95103492ae3c1a
lidiagaldino <lidiagabrielly14@gmail.com>
2024-07-29 16:00:30 -0300

    refactor: update GainRepository to use Optional for show method
    - Changed the return type of the show method in GainRepository to Optional<Gain>

--- src/main/java/com/lidiagaldino/finances/domain/repositories/GainRepository.java
+++ src/main/java/com/lidiagaldino/finances/domain/repositories/GainRepository.java
@@ -1,12 +1,14 @@
 package com.lidiagaldino.finances.domain.repositories;
 
 import com.lidiagaldino.finances.domain.entities.Gain;
+
+import java.util.Optional;
 import java.util.Set;
 
 public interface GainRepository {
   Gain save(Gain gain);
   void delete(Integer id);
   Gain update(Gain gain);
-  Gain show(Integer id);
+  Optional<Gain> show(Integer id);
   Set<Gain> list(Integer userId);
 }
